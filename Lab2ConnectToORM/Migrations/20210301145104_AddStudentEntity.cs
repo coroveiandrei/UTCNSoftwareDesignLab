21a83846296eb805aac4e546357c3fc1117d40da
coroveiandrei <37452422+coroveiandrei@users.noreply.github.com>
2021-03-01 17:43:51 +0200

    Add files via upload

---
+++ Lab2ConnectToORM/Migrations/20210301145104_AddStudentEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab2ConnectToORM.Migrations
+{
+    public partial class AddStudentEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Lab2ConnectToORM/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Lab2ConnectToORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StudentContext context = new StudentContext())
+            {
+
+                var newstudent = new Student { Name = "Mara", DateOfBirth = DateTime.Now };
+                context.Students.Add(newstudent);
+                context.SaveChanges();
+
+                var students = context.Students.ToListAsync().Result;
+                foreach(var student in students)
+                {
+                    Console.WriteLine("Student: " + student.Name);
+                }
+                
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
