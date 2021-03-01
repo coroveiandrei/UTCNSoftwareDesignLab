104abd72d2d2ab02d2d21b7ad278376b84af01ce
coroveiandrei <37452422+coroveiandrei@users.noreply.github.com>
2021-03-01 17:45:21 +0200

    Add files via upload

---
+++ lab2/Lab2ConnectToORM/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2ConnectToORM
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=lab2ORM;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ lab2/Lab2ConnectToORM/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2ConnectToORM
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
