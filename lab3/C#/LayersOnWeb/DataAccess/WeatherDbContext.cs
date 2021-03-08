0e062efb2e7334112b1a8d83e088fadfac9841a8
Andrei Corovei <andrei.corovei@qubiz.com>
2021-03-08 16:08:20 +0200

    Added Layers for .net core for web with swagger

---
+++ lab3/C#/LayersOnWeb/DataAccess/WeatherDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class WeatherDbContext : DbContext
+    {
+        public WeatherDbContext(DbContextOptions<WeatherDbContext> options)
+       : base(options)
+        {
+        }
+        public DbSet<WeatherForecastEntity> WeatherEntities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=.;Database=dbweather;Trusted_Connection=True;");
+        }
+    }
+}
